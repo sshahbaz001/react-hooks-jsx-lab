104b601d09f9cb404d9f93e93af2c31ff1e71d1c
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-07-04 13:01:00 -0400

    add

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  
+  return (
+    <div id="about">
+      <h1>About Me</h1>
+      <p>look at what I made</p>
+     <img src={image} alt="I made this" /> 
+    </div>
+
+  ) 
 }
 
 export default About;
